4f551f5700123129c54ea76030cc4bb3bb119bb8
kyle.smallwood@gmail.com <kyle.smallwood@gmail.com>
2019-08-04 21:37:32 +0100

    added types to handle user data; modified the service function and main function

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,34 @@
 use futures::{future, Future};
 use hyper::{Body, Error, Method, Request, Response, Server, StatusCode};
 use hyper::service::service_fn;
+use slab::Slab;
+use std::sync::{Arc, Mutex};
+use std::fmt;
 extern crate futures;
 extern crate hyper;
+
+
+type UserId = u64;
+struct UserData;
+
+//Arc= atomic reference counter provides multiple references to single instance of data - mutex over slab of user data
+type UserDb = Arc<Mutex<Slab<UserData>>>;
+
 
 fn main() {
 
     let addr = ([127, 0, 0, 1], 8080).into();
     let builder = Server::bind(&addr);
+    let user_db = Arc::new(Mutex::new(Slab::new()));
+    let server = builder.serve(move || {
+        let user_db = user_db.clone();
+        service_fn(move |req| microservice_handler(req, &user_db))
+    });
+    let server = server.map_err(drop);
+    hyper::rt::run(server);
+}
 
-    let server = builder.serve(|| service_fn(microservice_handler));
-
-    fn microservice_handler(req: Request<Body>) -> impl Future<Item=Response<Body>, Error=Error>
+    fn microservice_handler(req: Request<Body>, user_db: &UserDb) -> impl Future<Item=Response<Body>, Error=Error>
     {
             match(req.method(), req.uri().path()){
                 (&Method::GET, "/") => {
@@ -27,10 +44,13 @@ fn main() {
         }
     }
 
-    let server = server.map_err(drop);
-    hyper::rt::run(server);
-}
-
+    fn response_with_code(status_code: StatusCode) -> Response<Body> {
+        Response::builder()
+            .status(status_code)
+            .body(Body::empty())
+            .unwrap()
+    }
+    
 const INDEX: &'static str = r#"
 <!doctype html>
 <html>
